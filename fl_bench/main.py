c4394586c8e68a8c2fa107f0bbec9cfdbeb6ab10
Mirko <mak1788@gmail.com>
2022-12-17 13:42:00 +0100

    Added module net

--- fl_bench/main.py
+++ fl_bench/main.py
@@ -14,9 +14,9 @@ from fl_bench.algorithms.fedbn import FedBN
 from fl_bench.algorithms.fedopt import FedOpt, FedOptMode
 
 from fl_bench import GlobalSettings
-from data import Datasets
-from fl_bench.data import DataSplitter, Distribution, FastTensorDataLoader
+from fl_bench.data import DataSplitter, Distribution, FastTensorDataLoader, Datasets
 from fl_bench.utils import plot_comparison
+from fl_bench.net import *
 from utils import OptimizerConfigurator, Log
 from evaluation import ClassificationEval
 
@@ -41,134 +41,6 @@ dataset_map = {
 # train_data, test_data = Datasets.MNIST()
 # train_data, test_data = Datasets.SVHN()
 # train_data, test_data = Datasets.MNISTM()
-
-class MLP(nn.Module):
-    def __init__(self, input_size=28*28, num_classes=10):
-        super(MLP, self).__init__()
-        self.input_size = input_size
-        self.fc1 = nn.Linear(input_size, 50)
-        #self.fc1_drop = nn.Dropout(0.2)
-        self.fc2 = nn.Linear(50, 50)
-        #self.fc2_drop = nn.Dropout(0.2)
-        self.fc3 = nn.Linear(50, num_classes)
-
-    def forward(self, x):
-        x = x.view(-1, self.input_size)
-        x = F.relu(self.fc1(x))
-        #x = self.fc1_drop(x)
-        x = F.relu(self.fc2(x))
-        #x = self.fc2_drop(x)
-        return F.log_softmax(self.fc3(x), dim=1)
-    
-    def forward_(self, x):
-        x = x.view(-1, self.input_size)
-        x = F.relu(self.fc1(x))
-        return x
-        #x = self.fc1_drop(x)
-        #x = F.relu(self.fc2(x))
-        #x = self.fc2_drop(x)
-        #return F.log_softmax(self.fc3(x), dim=1)
-
-
-class MLP_BN(nn.Module):
-    def __init__(self, input_size=28*28, num_classes=10):
-        super(MLP_BN, self).__init__()
-        self.input_size = input_size
-        self.fc1 = nn.Linear(input_size, 50)
-        self.bn1 = nn.BatchNorm1d(50)
-        self.fc2 = nn.Linear(50, 50)
-        self.bn2 = nn.BatchNorm1d(50)
-        self.fc3 = nn.Linear(50, num_classes)
-
-    def forward(self, x):
-        x = torchvision.transforms.functional.rgb_to_grayscale(x, num_output_channels=1)
-        x = x.reshape(x.shape[0], self.input_size)
-        x = F.relu(self.bn1(self.fc1(x)))
-        x = F.relu(self.bn2(self.fc2(x)))
-        return F.log_softmax(self.fc3(x), dim=1)
-    
-    def forward_(self, x):
-        x = torchvision.transforms.functional.rgb_to_grayscale(x, num_output_channels=1)
-        x = x.reshape(x.shape[0], self.input_size)
-        x = F.relu(self.bn1(self.fc1(x)))
-        return x
-        #x = self.fc1_drop(x)
-        #x = F.relu(self.fc2(x))
-        #x = self.fc2_drop(x)
-        #return F.log_softmax(self.fc3(x), dim=1)
-
-class DigitModel(nn.Module):
-    """
-    Model for benchmark experiment on Digits. 
-    """
-    def __init__(self, num_classes=10, **kwargs):
-        super(DigitModel, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, 5, 1, 2)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.conv2 = nn.Conv2d(64, 64, 5, 1, 2)
-        self.bn2 = nn.BatchNorm2d(64)
-        self.conv3 = nn.Conv2d(64, 128, 5, 1, 2)
-        self.bn3 = nn.BatchNorm2d(128)
-    
-        self.fc1 = nn.Linear(8192, 2048)
-        self.bn4 = nn.BatchNorm1d(2048)
-        self.fc2 = nn.Linear(2048, 512)
-        self.bn5 = nn.BatchNorm1d(512)
-        self.fc3 = nn.Linear(512, num_classes)
-
-
-    def forward(self, x):
-        x = F.relu(self.bn1(self.conv1(x)))
-        x = F.max_pool2d(x, 2)
-
-        x = F.relu(self.bn2(self.conv2(x)))
-        x = F.max_pool2d(x, 2)
-
-        x = F.relu(self.bn3(self.conv3(x)))
-
-        x = x.view(x.shape[0], -1)
-
-        x = self.fc1(x)
-        x = self.bn4(x)
-        x = F.relu(x)
-
-        x = self.fc2(x)
-        x = self.bn5(x)
-        x = F.relu(x)
-
-        x = self.fc3(x)
-        return F.log_softmax(x, dim=1)
-    
-# Define model
-class CNN(nn.Module):
-    def __init__(self):
-        super(CNN, self).__init__()
-        self.conv1 = nn.Sequential(         
-            nn.Conv2d(
-                in_channels=1,              
-                out_channels=16,            
-                kernel_size=5,              
-                stride=1,                   
-                padding=2,                  
-            ),                              
-            nn.ReLU(),                      
-            nn.MaxPool2d(kernel_size=2),    
-        )
-        self.conv2 = nn.Sequential(         
-            nn.Conv2d(16, 32, 5, 1, 2),     
-            nn.ReLU(),                      
-            nn.MaxPool2d(2),                
-        )
-        # fully connected layer, output 10 classes
-        self.out = nn.Linear(32 * 7 * 7, 10)
-    
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.conv2(x)
-        # flatten the output of conv2 to (batch_size, 32 * 7 * 7)
-        x = x.view(x.size(0), -1)       
-        output = self.out(x)
-        return output #, x    # return x for visualization
 
 
 N_CLIENTS = 5
@@ -302,77 +174,6 @@ def run(algorithm: str = typer.Argument(..., help='Algorithm to run'),
     fl_algo.run(10)
     logger.save(f'./log/{algorithm}_{dataset}_{distribution}.json')
 
-# data_splitter = DataSplitter(train_data.data / 255., 
-#                              train_data.targets, 
-#                              n_clients=N_CLIENTS, 
-#                              distribution=Distribution.LABEL_PATHOLOGICAL_SKEWED, 
-#                              batch_size=BATCH_SIZE)
-
-# fedavg = FedAVG(n_clients=N_CLIENTS,
-#                 n_rounds=N_ROUNDS, 
-#                 n_epochs=N_EPOCHS, 
-#                 batch_size=BATCH_SIZE, 
-#                 model=MODEL, 
-#                 optimizer_cfg=OptimizerConfigurator(torch.optim.SGD, lr=0.01), 
-#                 loss_fn=nn.CrossEntropyLoss(), 
-#                 elegibility_percentage=ELIGIBILITY_PERCENTAGE,
-#                 seed=42)
-
-# fedavg.init_parties(data_splitter, logger)
-# fedavg.run(10)
-
-# logger.save('./log/fedavg_noniid_path.json')
-
-# scaffold = SCAFFOLD(n_clients=N_CLIENTS,
-#                     n_rounds=N_ROUNDS, 
-#                     n_epochs=N_EPOCHS, 
-#                     batch_size=BATCH_SIZE, 
-#                     model=MLP(), 
-#                     optimizer_cfg=OptimizerConfigurator(ScaffoldOptimizer, lr=0.01), 
-#                     loss_fn=nn.CrossEntropyLoss(), 
-#                     elegibility_percentage=ELIGIBILITY_PERCENTAGE,
-#                     seed=42)
-
-# scaffold.init_parties(data_splitter, global_step=1, callback=logger)
-# scaffold.run(10)
-
-# logger.save('./log/scaffold_noniid_path.json')
-
-
-# fedprox = FedProx(n_clients=N_CLIENTS,
-#        n_rounds=N_ROUNDS, 
-#        n_epochs=N_EPOCHS, 
-#        batch_size=BATCH_SIZE, 
-#        model=MODEL, 
-#        client_mu = 0.1,
-#        optimizer_cfg=OptimizerConfigurator(torch.optim.SGD, lr=0.01), 
-#        loss_fn=nn.CrossEntropyLoss(), 
-#        elegibility_percentage=ELIGIBILITY_PERCENTAGE,
-#        seed=42)
-
-# fedprox.init_parties(data_splitter, logger)
-# fedprox.run(10)
-
-# logger.save('./log/fedprox_noniid_dir.json')
-
-# flhalf = FLHalf(n_clients=N_CLIENTS,
-#                 n_rounds=N_ROUNDS, 
-#                 client_n_epochs=N_EPOCHS, 
-#                 server_n_epochs=2,
-#                 client_batch_size=BATCH_SIZE, 
-#                 server_batch_size=BATCH_SIZE, 
-#                 model=MODEL, 
-#                 server_optimizer_cfg=OptimizerConfigurator(torch.optim.SGD, lr=0.01), 
-#                 client_optimizer_cfg=OptimizerConfigurator(torch.optim.SGD, lr=0.05), 
-#                 loss_fn=nn.CrossEntropyLoss(), 
-#                 private_layers=["fc1"],
-#                 elegibility_percentage=ELIGIBILITY_PERCENTAGE,
-#                 seed=42)
-
-# flhalf.init_parties(data_splitter, logger)
-# flhalf.run(10)
-
-# logger.save('./log/flhalf_noniid_dir.json')
 
 @app.command()
 def compare(algorithms: str=typer.Argument(..., help='Algorithms to compare'),
---
+++ fl_bench/net.py
@@ -0,0 +1,131 @@
+import torchvision
+import torch.nn as nn
+from torch.functional import F
+
+class MLP(nn.Module):
+    def __init__(self, input_size=28*28, num_classes=10):
+        super(MLP, self).__init__()
+        self.input_size = input_size
+        self.fc1 = nn.Linear(input_size, 50)
+        #self.fc1_drop = nn.Dropout(0.2)
+        self.fc2 = nn.Linear(50, 50)
+        #self.fc2_drop = nn.Dropout(0.2)
+        self.fc3 = nn.Linear(50, num_classes)
+
+    def forward(self, x):
+        x = x.view(-1, self.input_size)
+        x = F.relu(self.fc1(x))
+        #x = self.fc1_drop(x)
+        x = F.relu(self.fc2(x))
+        #x = self.fc2_drop(x)
+        return F.log_softmax(self.fc3(x), dim=1)
+    
+    def forward_(self, x):
+        x = x.view(-1, self.input_size)
+        x = F.relu(self.fc1(x))
+        return x
+        #x = self.fc1_drop(x)
+        #x = F.relu(self.fc2(x))
+        #x = self.fc2_drop(x)
+        #return F.log_softmax(self.fc3(x), dim=1)
+
+
+class MLP_BN(nn.Module):
+    def __init__(self, input_size=28*28, num_classes=10):
+        super(MLP_BN, self).__init__()
+        self.input_size = input_size
+        self.fc1 = nn.Linear(input_size, 50)
+        self.bn1 = nn.BatchNorm1d(50)
+        self.fc2 = nn.Linear(50, 50)
+        self.bn2 = nn.BatchNorm1d(50)
+        self.fc3 = nn.Linear(50, num_classes)
+
+    def forward(self, x):
+        x = torchvision.transforms.functional.rgb_to_grayscale(x, num_output_channels=1)
+        x = x.reshape(x.shape[0], self.input_size)
+        x = F.relu(self.bn1(self.fc1(x)))
+        x = F.relu(self.bn2(self.fc2(x)))
+        return F.log_softmax(self.fc3(x), dim=1)
+    
+    def forward_(self, x):
+        x = torchvision.transforms.functional.rgb_to_grayscale(x, num_output_channels=1)
+        x = x.reshape(x.shape[0], self.input_size)
+        x = F.relu(self.bn1(self.fc1(x)))
+        return x
+        #x = self.fc1_drop(x)
+        #x = F.relu(self.fc2(x))
+        #x = self.fc2_drop(x)
+        #return F.log_softmax(self.fc3(x), dim=1)
+
+class DigitModel(nn.Module):
+    """
+    Model for benchmark experiment on Digits. 
+    """
+    def __init__(self, num_classes=10, **kwargs):
+        super(DigitModel, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, 5, 1, 2)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.conv2 = nn.Conv2d(64, 64, 5, 1, 2)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 128, 5, 1, 2)
+        self.bn3 = nn.BatchNorm2d(128)
+    
+        self.fc1 = nn.Linear(8192, 2048)
+        self.bn4 = nn.BatchNorm1d(2048)
+        self.fc2 = nn.Linear(2048, 512)
+        self.bn5 = nn.BatchNorm1d(512)
+        self.fc3 = nn.Linear(512, num_classes)
+
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.max_pool2d(x, 2)
+
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.max_pool2d(x, 2)
+
+        x = F.relu(self.bn3(self.conv3(x)))
+
+        x = x.view(x.shape[0], -1)
+
+        x = self.fc1(x)
+        x = self.bn4(x)
+        x = F.relu(x)
+
+        x = self.fc2(x)
+        x = self.bn5(x)
+        x = F.relu(x)
+
+        x = self.fc3(x)
+        return F.log_softmax(x, dim=1)
+    
+# Define model
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Sequential(         
+            nn.Conv2d(
+                in_channels=1,              
+                out_channels=16,            
+                kernel_size=5,              
+                stride=1,                   
+                padding=2,                  
+            ),                              
+            nn.ReLU(),                      
+            nn.MaxPool2d(kernel_size=2),    
+        )
+        self.conv2 = nn.Sequential(         
+            nn.Conv2d(16, 32, 5, 1, 2),     
+            nn.ReLU(),                      
+            nn.MaxPool2d(2),                
+        )
+        # fully connected layer, output 10 classes
+        self.out = nn.Linear(32 * 7 * 7, 10)
+    
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        # flatten the output of conv2 to (batch_size, 32 * 7 * 7)
+        x = x.view(x.size(0), -1)       
+        output = self.out(x)
+        return output #, x    # return x for visualization
