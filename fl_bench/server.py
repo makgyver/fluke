1b3b7806c2265dc6994c1544186dcab47a835e8c
Mirko <mak1788@gmail.com>
2023-01-03 17:15:38 +0100

    Added documentation to server.py

--- fl_bench/server.py
+++ fl_bench/server.py
@@ -38,6 +38,13 @@ class Server(ABC):
         self.callbacks = []
     
     def fit(self, n_rounds: int=10) -> None:
+        """Run the federated learning algorithm.
+
+        Parameters
+        ----------
+        n_rounds : int, optional
+            The number of rounds to run, by default 10.
+        """
         with Progress() as progress:
             client_x_round = int(self.n_clients*self.elegibility_percentage)
             task_rounds = progress.add_task("[red]FL Rounds", total=n_rounds*client_x_round)
@@ -57,21 +64,58 @@ class Server(ABC):
                 self.notify_all(self.model, round + 1, client_evals)
 
     def get_eligible_clients(self) -> Iterable[Client]:
+        """Get the clients that will participate in the current round.
+
+        The number of clients is determined by the `elegibility_percentage` attribute.
+
+        Returns
+        -------
+        Iterable[Client]
+            The clients that will participate in the current round.
+        """
         if self.elegibility_percentage == 1:
             return self.clients
         n = int(self.n_clients * self.elegibility_percentage)
         return np.random.choice(self.clients, n)
 
     def broadcast(self, eligible: Iterable[Client]=None) -> None:
+        """Broadcast the model to the clients.
+
+        The broadcast is done by calling the `receive` method of each participating client.
+
+        Parameters
+        ----------
+        eligible : Iterable[Client], optional
+            The clients that will receive the model, by default None. 
+            If None, all clients will receive the model.
+        """
         eligible = eligible if eligible is not None else self.clients
         for client in eligible:
             client.receive(deepcopy(self.model))
 
     def init(self, path: str=None, **kwargs) -> None:
+        """Initialize the server model from a checkpoint.
+
+        Parameters
+        ----------
+        path : str, optional
+            The path to the checkpoint, by default None. 
+            If None, the model will be initialized randomly.
+        """
         if path is not None:
             self.load(path)
     
     def aggregate(self, eligible: Iterable[Client]) -> None:
+        """Aggregate the models of the clients.
+
+        The aggregation is done by averaging the models of the clients. If the attribute `weighted` 
+        is True, the clients are weighted by their number of samples.
+
+        Parameters
+        ----------
+        eligible : Iterable[Client]
+            The clients whose models will be aggregated.
+        """
         avg_model_sd = OrderedDict()
         clients_sd = [eligible[i].send().state_dict() for i in range(len(eligible))]
         with torch.no_grad():
@@ -91,15 +135,37 @@ class Server(ABC):
             self.model.load_state_dict(avg_model_sd)
     
     def register_callback(self, callback: Callable) -> None:
+        """Register a callback to be called after each round.
+
+        Parameters
+        ----------
+        callback : Callable
+            The callback to be registered.
+        """
         if callback is not None and callback not in self.callbacks:
             self.callbacks.append(callback)
     
     def notify_all(self, *args, **kwargs) -> None:
+        """Notify all registered callbacks."""
         for callback in self.callbacks:
             callback(*args, **kwargs)
     
     def save(self, path: str) -> None:
+        """Save the model to a checkpoint.
+
+        Parameters
+        ----------
+        path : str
+            The path to the checkpoint.
+        """
         torch.save(self.model.state_dict(), path)
     
     def load(self, path: str) -> None:
+        """Load the model from a checkpoint.
+
+        Parameters
+        ----------
+        path : str
+            The path to the checkpoint.
+        """
         self.model.load_state_dict(torch.load(path))
