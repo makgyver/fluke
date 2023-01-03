f7f0b3d0354d8074873587b77085e9b728ed4ed9
Mirko <mak1788@gmail.com>
2023-01-03 17:17:40 +0100

    Update __init__.py

--- fl_bench/__init__.py
+++ fl_bench/__init__.py
@@ -1,6 +1,7 @@
 import torch
 
 class Singleton(type):
+    """Singleton metaclass."""
     _instances = {}
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
@@ -8,16 +9,44 @@ class Singleton(type):
         return cls._instances[cls]
 
 
-class GlobalSettings(metaclass=Singleton):    
+class GlobalSettings(metaclass=Singleton):
+    """Global settings for the library.""" 
+    
     _device = 'cpu'
 
-    def auto_device(self):
+    def auto_device(self) -> torch.device:
+        """Set device to cuda if available, otherwise cpu.
+        
+        Returns
+        -------
+        torch.device
+            The device.
+        """
         self._device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         return self._device
     
     def set_device(self, device):
+        """Set the device.
+        
+        Parameters
+        ----------
+        device : torch.device
+            The device to set.
+        
+        Returns
+        -------
+        torch.device
+            The device.
+        """
         self._device = device
         return self._device
     
     def get_device(self):
+        """Get the device.
+
+        Returns
+        -------
+        torch.device
+            The device.
+        """
         return self._device
