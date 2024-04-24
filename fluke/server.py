e32ee52a5acfbf62c759e351dc5fa4dc1a24dbd4
Mirko <mak1788@gmail.com>
2024-04-24 16:14:28 +0200

    Update server.py

--- fluke/server.py
+++ fluke/server.py
@@ -148,7 +148,7 @@ class Server(ObserverSubject):
         """
         if self.test_data is not None:
             return ClassificationEval(self.clients[0].hyper_params.loss_fn,
-                                      self.model.output_size,
+                                      self.hyper_params.n_protos,
                                       device=GlobalSettings().get_device()).evaluate(self.model,
                                                                                      self.test_data)
         return {}
