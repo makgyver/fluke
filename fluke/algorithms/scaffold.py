33ae21bc296dff1b376d8a6a16b0a7e41c83cb13
Mirko <mak1788@gmail.com>
2024-08-09 19:26:45 +0200

    Update scaffold.py

--- fluke/algorithms/scaffold.py
+++ fluke/algorithms/scaffold.py
@@ -98,6 +98,11 @@ class SCAFFOLDClient(Client):
                 y_hat = self.model(X)
                 loss = self.hyper_params.loss_fn(y_hat, y)
                 loss.backward()
+                for param, c, c_i in zip(self.model.parameters(),
+                                         self.server_control,
+                                         self.control):
+                    if param.requires_grad:
+                        param.grad.data += (c - c_i).to(self.device)
                 self.optimizer.step()  # self.server_control, self.control)
             self.scheduler.step()
 
