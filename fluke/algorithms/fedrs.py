785c60d54f1ad79009c93295bf4284eabc9fa331
Mirko <mak1788@gmail.com>
2024-08-19 11:07:58 +0200

    Added FedRS
    Former-commit-id: e6f10b81e967ffb04cda663e28ec087c9ef3c64f

---
+++ fluke/algorithms/fedrs.py
@@ -0,0 +1,66 @@
+"""Implementation of the [FedRS21]_ algorithm.
+
+References:
+    .. [FedRS21] Xin-Chun Li and De-Chuan Zhan. 2021. FedRS: Federated Learning with Restricted
+       Softmax for Label Distribution Non-IID Data. In: KDD (2021). 
+       URL: https://doi.org/10.1145/3447548.3467254
+"""
+import torch
+import numpy as np
+import sys
+sys.path.append(".")
+sys.path.append("..")
+
+from ..utils import OptimizerConfigurator  # NOQA
+from ..client import Client  # NOQA
+from ..data import FastDataLoader  # NOQA
+from . import CentralizedFL  # NOQA
+
+
+class RSLoss(torch.nn.Module):
+
+    def __init__(self, class_scaling: torch.Tensor):
+        super().__init__()
+        self.class_scaling = class_scaling
+
+    def forward(self, logit: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+        out = logit * self.class_scaling
+        return torch.nn.functional.cross_entropy(out, y)
+
+    def __str__(self):
+        return "RSLoss()"
+
+    def __repr__(self):
+        return str(self)
+
+
+class FedRSClient(Client):
+
+    def __init__(self,
+                 index: int,
+                 train_set: FastDataLoader,
+                 test_set: FastDataLoader,
+                 optimizer_cfg: OptimizerConfigurator,
+                 loss_fn: torch.nn.Module,  # ignored
+                 local_epochs: int,
+                 alpha: float,
+                 count_as_missing: int = 2,
+                 **kwargs):
+        super().__init__(index=index, train_set=train_set, test_set=test_set,
+                         optimizer_cfg=optimizer_cfg, loss_fn=None, local_epochs=local_epochs,
+                         **kwargs)
+        self.hyper_params.update(alpha=alpha, count_as_missing=count_as_missing)
+        uniq_val, uniq_cnt = np.unique(self.train_set.tensors[1], return_counts=True)
+        class_scaling = torch.ones(self.train_set.num_labels) * \
+            self.hyper_params.alpha
+        for i, c in enumerate(uniq_val):
+            if uniq_cnt[i] > self.hyper_params.count_as_missing:
+                class_scaling[c] = 1.0
+        class_scaling = class_scaling.unsqueeze(dim=0).to(self.device)
+        self.hyper_params.loss_fn = RSLoss(class_scaling)
+
+
+class FedRS(CentralizedFL):
+
+    def get_client_class(self) -> Client:
+        return FedRSClient
