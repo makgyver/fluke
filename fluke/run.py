a7a1aecbdf33d2cc86be10ca02984e7f4f3503fe
Mirko <mak1788@gmail.com>
2024-10-04 16:50:25 +0200

    Release 0.3.3

--- fluke/run.py
+++ fluke/run.py
@@ -21,7 +21,7 @@ from .utils import (Configuration, OptimizerConfigurator,  # NOQA
                     get_class_from_qualified_name, get_loss, get_model)
 from .utils.log import get_logger  # NOQA
 
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 
 
 def version_callback(value: bool):
