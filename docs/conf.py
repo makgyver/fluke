91fbb4a8ae41c4867c03b1cccc3dd84921bf56d4
Mirko <mak1788@gmail.com>
2025-05-06 17:53:10 +0200

    Release v0.7.4

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 project = 'fluke'
 copyright = '2024, Mirko Polato'
 author = 'Mirko Polato'
-version = '0.7.3'
+version = '0.7.4'
 # release = 'alpha'
 
 # autodoc_mock_imports = ['algorithms'] # Fix the "No module named 'algorithms'" error
